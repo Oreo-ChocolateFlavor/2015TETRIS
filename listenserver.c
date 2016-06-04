84c22dd9514700fa8714753c22429d128d98fbc0
kim sang hyun <sosfd3849@gmail.com>
2016-06-04 22:04:45 +0900

    Connected Server 구현 시

--- listenserver.c
+++ listenserver.c
@@ -9,19 +9,23 @@
 #include <string.h>
 #include <stdbool.h>
 
+
 #define CREATEROOM_SIGNAL -100   // 클라이언트에서 방을 만들때!
 #define ROOMINFOSEND_SIGNAL -101 // 방정보를 보내달라!
 #define ADDROOM_SIGNAL -102  // 이건 클라가 보낼일이 없음!
 #define JOINROOM_SIGNAL -103    // 방을 조인할때
 #define CLOSE_MAINROOM_SIGNAL -104 // 대기방을 close 할떄
+#define PORT_SIG -105;
 
+int portnumber = 7000;
 int inputsignal;
 #define SIG inputsignal
 int nfd,fdname;
 
 struct PIPE
 {
-  int fd[2];
+  int child[2];
+  int parent[2];
 }p[1000];
 
 int pipe_count;
@@ -37,14 +41,22 @@ struct room_info
 static void sighandler(int sig)
 {
   printf("killed!\n");
+  int status;
+  pid_t id = waitpid(-1,&status,WNOHANG);
+
+  if(WIFEXITED(status))
+  {
+    printf("ZOMBIE is killed\n");
+  }
+
 }
-void SendRoomList(int* pip);
-void AddRoomList(int* pip);
-void Gameserver(int* pip);
+void SendRoomList(struct PIPE pip);
+void AddRoomList(struct PIPE pip);
+void Gameserver(struct PIPE pip,int connectedsock);
 void ReadMessage(int sock,char* buf);
-void ConnectedServer(int connectedsock,int* pip);
-void CreateRoom(int* pip);
-void JoinRoom(int* pip);
+void ConnectedServer(int connectedsock,struct PIPE pip);
+void CreateRoom(struct PIPE pip);
+void JoinRoom(struct PIPE pip);
 
 int main(int argc,char* argv[])
 {
@@ -118,19 +130,26 @@ int main(int argc,char* argv[])
     {
 
       client_sock = accept(server_sock,(struct sockaddr*)&clientaddr,(socklen_t*)&len);
-
-      int pipe_sock[2];
-      pipe(pipe_sock);
-
-      p[pipe_count].fd[0] = pipe_sock[0];
-      p[pipe_count].fd[1] = pipe_sock[1];
-      pipe_count++;
-      FD_SET(pipe_sock[0],&oldset);
-
-      if(nfd < pipe_sock[0])
+      int child[2],parent[2];
+      if(pipe(child) == -1)
       {
-        nfd = pipe_sock[0] +1;
+        perror("pipe");
       }
+      if(pipe(parent)==-1)
+      {
+        perror("pipe");
+      }
+
+      p[pipe_count].child[0] = child[0]; p[pipe_count].child[1] = child[1];
+      p[pipe_count].parent[0] = parent[0]; p[pipe_count].parent[1] = parent[1];
+
+      FD_SET(p[pipe_count].parent[0],&oldset);
+
+      if(nfd < p[pipe_count].parent[0])
+      {
+        nfd = p[pipe_count].parent[0]+1;
+      }
+
 
       if(client_sock == -1)
       {
@@ -142,7 +161,7 @@ int main(int argc,char* argv[])
       if(pid == 0)  // 자식이면! 포크후 커넥티트 서버라는 함수를 실행하는 프로세스를 만듬!
       {
         close(server_sock);
-        ConnectedServer(client_sock,pipe_sock);
+        ConnectedServer(client_sock,p[pipe_count]);
       }
       else if(pid > 0) // 부모면!
       {
@@ -153,25 +172,38 @@ int main(int argc,char* argv[])
         perror("fork()");
         exit(1);
       }
+
+      pipe_count++;
     }
     else{
       for(int i=0; i<pipe_count; i++)
       {
-        if(FD_ISSET(p[0].fd[0],&newset)) // 자식 프로세스로 부터 통신 요청이 들어오면!
+        if(FD_ISSET(p[i].parent[0],&newset)) // 자식 프로세스로 부터 통신 요청이 들어오면!
         {
-          char pipemessagebuf[1024];
-          ReadMessage(p[i].fd[0],pipemessagebuf);
+          char pipemessagebuf[100] = "";
+          ReadMessage(p[i].parent[0],pipemessagebuf);
+
+          printf("In the Mainserver recv SIG : %d\n",SIG);
 
           if(SIG == CLOSE_MAINROOM_SIGNAL) // 자식이 EXIT했다는 거임.
           {
-            FD_CLR(p[i].fd[0],&oldset);
-            p[i].fd[0] = p[pipe_count-1].fd[0];
-            p[i].fd[1] = p[pipe_count-1].fd[1];
+            printf("Mainserver: CLOSE MAINROOM_SIGNAL\n");
+            FD_CLR(p[i].parent[0],&oldset);
+            p[i] = p[pipe_count];
             pipe_count--;
           }
           else if(SIG == JOINROOM_SIGNAL)
           {
 
+          }
+          else if(SIG == CREATEROOM_SIGNAL) // 여기서 포트넘버를 넘겨주고 포트를 증가시킨다.
+          {
+            printf("Mainserver: CREATEROOM_SIGNAL recv\n");
+            write(p[i].child[1],&portnumber,sizeof(int));
+            portnumber++;
+          }
+          else{
+            printf("Nah... error\n");
           }
         }
       }
@@ -185,25 +217,26 @@ int main(int argc,char* argv[])
 }
 
 
-void SendRoomList(int* pip)
+void SendRoomList(struct PIPE pip)
 {
   int sig = ROOMINFOSEND_SIGNAL;
 
-  write(pip[1],&sig,1); //부모 서버로 요청을 보냄
-  read() // 자식은 정보를 읽어드린후
-  write() // 클라이언트한테 보냄;
+//  write(pip[1],&sig,1); //부모 서버로 요청을 보냄
+//  read() // 자식은 정보를 읽어드린후
+//  write() // 클라이언트한테 보냄;
 
   printf("%d in the SendRoomList()\n",getpid());
 }
 
-void AddRoomList(int* pip)
+void AddRoomList(struct PIPE pip)
 {
   printf("%d in the AddRoomList()\n",getpid());
 }
 
-void Gameserver(int* pip)
+void Gameserver(struct PIPE pip)
 {
-    printf("%d in the Gameserver()\n",getpid());
+  bind();
+
 }
 
 void ReadMessage(int sock,char* buf)
@@ -221,37 +254,60 @@ void ReadMessage(int sock,char* buf)
   }
 
   SIG = *(t-1);
-  printf("<%d> is read  SIG is  = %d  \"%s\"\n",getpid(),SIG,buf);
-
   *(t-1) = 0;
+  printf("<%d> is read  SIG is  = %d  \"%s\"\n",getpid(),SIG,buf);
 }
 
-void JoinRoom(int* pip)
+void JoinRoom(struct PIPE pip)
 {
-  printf("%d in the JoinRoom()\n",getpid());
+//  printf("%d in the JoinRoom()\n",getpid());
 }
 
-void CreateRoom(int* pip)
+void CreateRoom(struct PIPE pip,int connectedsock) // 여기서 게임방 fork를 해준후에  서버랑 포트넘버를 넘겨줌;
 {
-  printf("%d in the CreateRoom()\n",getpid());
+  char sig = CREATEROOM_SIGNAL;
+  int gameserverport;
+
+  pid_t gameserver_pid;
+
+
+  // 여기 방정보를 서버를 보내는 코드를 넣는다.
+  write(pip.parent[1],&sig,1);
+
+  //서버 포트를 받고고
+  read(pip.child[0],&gameserverport,sizeof(int));
+  write(connectedsock,&gameserverport,sizeof(int));
+
+  if((gameserver_pid = fork()) < 0)
+  {
+    perror("gameserver fork()");
+  }else if(gameserver_pid == 0) // 여기가 게임서버!
+  {
+    Gameserver(pip);
+  }
+  else if(gameserver_pid > 0) //  여기는 부모!
+  {
+
+  }
+
+  printf("%d in the CreateRoom() %d\n",getpid(),gameserverport);
 }
 
-void ConnectedServer(int connectedsock,int* pip) //커넥트 된후 실행되는 놈.
+void ConnectedServer(int connectedsock,struct PIPE pip) //커넥트 된후 실행되는 놈.
 {
   char buf[1024];
   while(1)
   {
     memset(buf,0,sizeof(buf));
     ReadMessage(connectedsock,buf);
-    if(SIG == ROOMINFOSEND_SIGNAL){ printf("TEST ROOMINFOSEND_SIGNAL\n"); SendRoomList(pip); }
-    else if(SIG == ADDROOM_SIGNAL){ printf("TEST ADDROOM_SIGNAL\n"); AddRoomList(pip); }
-    else if(SIG == CREATEROOM_SIGNAL){ printf("TEST CREATEROOM_SIGNAL\n"); CreateRoom(pip); }
-    else if(SIG == JOINROOM_SIGNAL){ printf("TEST JOINROOM_SIGNAL\n"); JoinRoom(pip); }
-    else if(SIG ==  CLOSE_MAINROOM_SIGNAL)
+    if(SIG == ROOMINFOSEND_SIGNAL){ printf("TEST ROOMINFOSEND_SIGNAL\n"); SendRoomList(pip);}
+    else if(SIG == CREATEROOM_SIGNAL) {printf("TEST CREATEROOM_SIGNAL\n"); CreateRoom(pip,connectedsock);}
+    else if(SIG == JOINROOM_SIGNAL){printf("TEST JOINROOM_SIGNAL\n"); JoinRoom(pip); }
+    else if(SIG == CLOSE_MAINROOM_SIGNAL)
     {
-      printf("READ END SIGNAL\n");
+      printf("<PID: %d>READ END SIGNAL %d\n",getpid(),pip.parent[1]);
       char endsignal = CLOSE_MAINROOM_SIGNAL;
-      write(pip[1],&endsignal,1);
+      write(pip.parent[1],&endsignal,1);
       close(connectedsock);
       exit(1);
     }
